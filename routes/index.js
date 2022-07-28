71dbd248e9f47fdc178de0a6bbc9f815b40d7204
lmj <mmj9808@naver.com>
2022-07-28 16:16:40 +0900

    REFACTOR: 필요없는 파일 삭제

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as example from './example.routes.js';
-
-const router = Router();
-
-router.use(example.path, example.router);
-
-
-export default router;
