b55516d2830554786f3289649305f8e106e68636
minleejae <mmj9808@naver.com>
2022-08-04 01:43:19 +0900

    FEATURE: HOME 컴포넌트 요소 추가 및 이미지 변경

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,23 @@
+import homeImage from "./../images/home.jpeg";
+
 const Home = () => {
-    return (
-        <div>
-            <h1>
-                Description about webpage
-            </h1>
-            <img src={"https://cdn.pixabay.com/photo/2017/04/06/11/24/fashion-2208045_960_720.jpg"} style={{ width: 100 + '%' }} />
+  return (
+    <div>
+      <h1>Description about webpage</h1>
+      <div style={{ position: "relative" }}>
+        <img
+          src={homeImage}
+          alt="home-main"
+          style={{ width: 100 + "%", marginTop: -300 + "px" }}
+        />
+        <div style={{ position: "absolute", top: 30 + "%", left: 5 + "%" }}>
+          <h1 style={{ fontSize: 400 + "%", color: "white" }}>
+            What is AI CLOSET?
+          </h1>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
