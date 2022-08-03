db9069e03b179a0fe96d96bbf30a09461284f7c8
heungkuk <hkyoo988@naver.com>
2022-08-04 00:46:03 +0900

    FEATURE: posts list route 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 // import * as example from './example.routes.js';
 import * as users from './users.routes.js';
 import * as closet from './closet.routes.js';
+import * as posts from './posts.routes.js';
 
 const router = Router();
 
 // router.use(example.path, example.router);
 router.use(users.path, users.router);
 router.use(closet.path, closet.router);
+router.use(posts.path, posts.router);
 
 
 export default router;
