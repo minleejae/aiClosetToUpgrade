b4535463c06ca484e9a57dfd8afc840fd2ccdd19
seunam <seunam@student.42seoul.kr>
2022-08-11 12:19:32 +0900

    CHORE: 불필요한 임포트 제거 및 서치 루트 추가

---
+++ server/routes/search.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { Post } from '../models/index.js'
+
+export const path = '/search';
+export const router = Router();
+
+router.get("/", async (req, res, next) => {
+    
+});
