c4a1231d6d8ebabc6b2f2bf6af63e997d6cbd160
seunam <seunam@student.42seoul.kr>
2022-08-13 21:29:49 +0900

    FEATURE: socket.js added

--- server/app.js
+++ server/app.js
@@ -4,15 +4,17 @@ import mongoose from 'mongoose';
 import {createServer} from 'http';
 import {Server} from 'socket.io';
 import cors from 'cors';
+import socket from './util/socket.js';
 
 const app = express();
-export const server = createServer(app);
+const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     credentials: true,
   },
 });
+
 
 mongoose.connect("mongodb://localhost:27017/Ocloset");
 
@@ -27,6 +29,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('images'));
+
+// socket 파일에 객체 전달
+socket(io);
 
 
 app.get('/welcome', (req, res) => {
