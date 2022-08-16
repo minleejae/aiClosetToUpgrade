c90ca27a30be2f10ac31191cc4ad670489812174
minleejae <mmj9808@naver.com>
2022-08-17 02:39:19 +0900

    FEAUTRE :DB 옷 이미지에 styleType 추가

--- server/models/schemas/post.js
+++ server/models/schemas/post.js
@@ -1,44 +1,49 @@
-import mongoose from "mongoose"
-import shortId from "./type/shortId.js"
+import mongoose from "mongoose";
+import shortId from "./type/shortId.js";
 
-const PostSchema = new mongoose.Schema({
+const PostSchema = new mongoose.Schema(
+  {
     shortId,
     title: String,
     content: String,
     show: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
     views: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     price: Number, //당마용
-    // 옷장:1, OOTD:2, 당근마켓:3 
+    // 옷장:1, OOTD:2, 당근마켓:3
     postType: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
-    // url: 이미지 주소, category: [top, bottom, shoes]
+    // url: 이미지 주소, category: [top, bottom, shoes], style: suit/ casual
     img: {
-        url: {
-            type: String,
-            required: true
-        },
-        category: String
+      url: {
+        type: String,
+        required: true,
+      },
+      category: String,
+      style: String,
     },
     author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    comments: [{
+    comments: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Upment",
-    }],
-}, {
-    timestamps: true
-}
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
 export default PostSchema;
